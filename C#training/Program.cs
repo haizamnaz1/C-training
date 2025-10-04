5064b7ddc600b6da114a1207707814a026446f83
haizamnaz1 <haizamnazreen@gmail.com>
2025-10-04 21:15:17 +0530

    E1 - understanding what each line of code does.

--- C#training/Program.cs
+++ C#training/Program.cs
@@ -1,12 +1,12 @@
-﻿using System;
+﻿using System; //this measns we are using the System namespace
 
-namespace HelloWorld
+namespace HelloWorld //used to organize my code and its like a container for classes and other namespaces
 {
-    class Program
+    class Program //this is a class named Program and every code must be written inside a class.
     {
-        static void Main(string[] args)
+        static void Main(string[] args) //this is the main method, the entry point of the program
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!"); //console is a class in the System namespace and Writeline is a method that prints text to the console.
         }
     }
 }

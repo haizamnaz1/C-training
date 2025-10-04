98d8e9f7f90353a054abdd7085a8bf71be50c039
haizamnaz1 <haizamnazreen@gmail.com>
2025-10-04 21:23:24 +0530

    E2 - Outputs and comments

--- C#training/Program.cs
+++ C#training/Program.cs
@@ -7,6 +7,16 @@ namespace HelloWorld //used to organize my code and its like a container for cla
         static void Main(string[] args) //this is the main method, the entry point of the program
         {
             Console.WriteLine("Hello World!"); //console is a class in the System namespace and Writeline is a method that prints text to the console.
+            Console.WriteLine("Im Learning C# yayayay");
+            Console.WriteLine("Im excited to learn more about C#");
+            //we cam also add numbers in teh console writeline
+            Console.WriteLine(5 + 10);
+            Console.WriteLine(5 * 10);
+
+            //thers a new method called write and the difference is it doesnt add a new line after printing
+            Console.Write("Hello ");
+            Console.Write("World!");
+
         }
     }
 }
